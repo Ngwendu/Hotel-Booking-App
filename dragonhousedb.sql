5999df262ae4d8ab9f7253dee65860afbe2c080f
Ngwendu <gambungwendu@gmail.com>
2022-11-21 17:40:49 +0200

    Admin

--- dragonhousedb.sql
+++ dragonhousedb.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 21, 2017 at 06:16 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
